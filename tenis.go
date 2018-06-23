bf4ad213fab73b19b7fb2a4bda2b165f9c7bb046
ireneginani <ireneginani@gmail.com>
2018-06-22 22:36:40 -0300

    versão onde temos com pontos, games e set

--- tenis.go
+++ tenis.go
@@ -2,7 +2,7 @@ package main
 import(
 	"fmt"
 	"math/rand"
-	"time"
+	//"time"
 	"sync"
 )
 
@@ -15,7 +15,7 @@ func jogador1(c chan bool){
 
 	perdeu := false
 	
-	rand.Seed(time.Now().Unix())
+	rand.Seed(0)
 	bola := random(1, 3)
 	//fmt.Println(bola)
 	if bola == 1 {
@@ -30,7 +30,7 @@ func jogador2(c chan bool){
 
 	perdeu := false
 	
-	rand.Seed(time.Now().Unix())
+	rand.Seed(1)
 	bola := random(1, 3)
 	//fmt.Println(bola)
 	if bola == 1 {
@@ -45,55 +45,93 @@ func main() {
 
 	
 	var mutex = &sync.Mutex{}
-	var pontos int
+	var pontos, game, set int
 	//fmt.Println("Digite de quantos pontos é feito o game")
 	
 	//fmt.Scan(&pontos)
 	
-	pontos = 5;
+	pontos = 5
+	game = 3
+	set = 3
 	c := make(chan bool)
 	
 	
 	pontos_jogador1 := 0
 	pontos_jogador2 := 0
 	
-	for pontos_jogador1 < pontos && pontos_jogador2 < pontos {
-		
-		//jogador 1 passando a bola
-		
-		mutex.Lock()
-		go jogador1(c)
-		mutex.Unlock()
-		
-		//teste para saber se o jogador 1 perdeu a bola
+	 games_jogador1 := 0
+	 games_jogador2 := 0
 	
-		if <-c {
+	 set_jogador1 := 0
+	 set_jogador2 := 0
+
+	 for set_jogador1 < set && set_jogador2 < set {
 		
-		   pontos_jogador2 = pontos_jogador2 + 1
-		   
-		   
-		}
-		
-		//jogador 2 passando a bola
-		
-		mutex.Lock()
-		go jogador2(c)
-		mutex.Unlock()
-		
-		//teste para saber se o jogador 2 pedeu a bola
-		
-		if <-c {
-		   
-		   pontos_jogador1 = pontos_jogador1 + 1
-		   
-		}
+		for games_jogador1 < pontos && games_jogador2 < game {
 	
-		fmt.Println("pontos 1:")
-		fmt.Println(pontos_jogador1)
-		fmt.Println("pontos 2:")
-		fmt.Println(pontos_jogador2)
-		//pontos_jogador1 = 5
-	
-	}
-	
+			for pontos_jogador1 < pontos && pontos_jogador2 < pontos {
+				
+				//jogador 1 passando a bola
+				
+				mutex.Lock()
+				go jogador1(c)
+				mutex.Unlock()
+				
+				//teste para saber se o jogador 1 perdeu a bola
+			
+				if <-c {
+				
+				   pontos_jogador2 = pontos_jogador2 + 1
+				   
+				   
+				}
+				
+				//jogador 2 passando a bola
+				
+				mutex.Lock()
+				go jogador2(c)
+				mutex.Unlock()
+				
+				//teste para saber se o jogador 2 pedeu a bola
+				
+				if <-c {
+				   
+				   pontos_jogador1 = pontos_jogador1 + 1
+				   
+				}
+			
+				fmt.Println("pontos 1:")
+				fmt.Println(pontos_jogador1)
+				fmt.Println("pontos 2:")
+				fmt.Println(pontos_jogador2)
+				//pontos_jogador1 = 5
+			
+			}
+
+	 		if pontos_jogador1 == pontos {
+	 			games_jogador1 = games_jogador1 + 1
+	 		} else if pontos_jogador2 == pontos {
+	 			games_jogador2 = games_jogador2 + 1
+	 		}
+
+	 		pontos_jogador2 = 0
+	 		pontos_jogador1 = 0
+
+	 	}
+
+	 	if games_jogador1 == game {
+	 			set_jogador1 = set_jogador1 + 1
+	 		} else if games_jogador2 == game {
+	 			set_jogador2 = set_jogador2 + 1
+	 		}
+
+	 		games_jogador2 = 0
+	 		games_jogador1 = 0
+	 }
+
+	 if set_jogador1 == set {
+	 			fmt.Println("jogador 1 venceu")
+	 		} else if set_jogador2 == set {
+	 			fmt.Println("jogador 2 venceu")
+	 		}
 }
